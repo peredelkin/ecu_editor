89d347d5cfc2ad458d9455822cf802f6c2e8a245
Ruslan Stoyanov <ep8kha@gmail.com>
2020-07-30 12:31:02 +0500

    градиент

--- qtablemodel_ign_angle_mg_by_cycle.h
+++ qtablemodel_ign_angle_mg_by_cycle.h
@@ -50,8 +50,8 @@ private:
 
     static int bsearch_compare(const void *ap, const void *bp)
     {
-        const float *a = (float *) ap;
-        const float *b = (float *) bp;
+        const qreal *a = (qreal *) ap;
+        const qreal *b = (qreal *) bp;
         if(*a < b[0])
             return -1;
         else if(*a >= b[1])
@@ -60,7 +60,7 @@ private:
             return 0;
     }
 
-    const float scale_table[7] =  {
+    const qreal scale_table[7] =  {
         -5,
         5,
         15,
@@ -69,33 +69,23 @@ private:
         45,
         55};
 
-    //const color_t color_table[7] = {
-    //    {255,0,255},    /*фиолетовый*/
-    //    {0,0,255},      /*синий*/
-    //    {0,255,255},    /*голубой*/
-    //    {0,255,0},      /*зеленый*/
-    //    {255,255,0},    /*желтый*/
-    //    {255,0,0},      /*красный*/
-    //    {255,255,255}   /*белый*/
-    //};
-
     const QColor color_table[7] = {
-        QColor(255,0,255),
-        QColor(0,0,255),
-        QColor(0,255,255),
-        QColor(0,255,0),
-        QColor(255,255,0),
-        QColor(255,0,0),
-        QColor(255,255,255),
+        QColor(255,0,255,100),
+        QColor(0,0,255,100),
+        QColor(0,255,255,100),
+        QColor(0,255,0,100),
+        QColor(255,255,0,100),
+        QColor(255,0,0,100),
+        QColor(255,255,255,100),
     };
 
     qreal gradient_interpolation(const qreal color0,const qreal color1,const qreal factor) const {
         return (color0 + ((color1 - color0) * factor));
     }
 
-    QColor gradient_by_value(const float value,const QColor* color,const float* scale,int scale_count) const  {
-        float val = min_max_float(value,scale[0],scale[scale_count-2]);
-        float *val_point = (float*) bsearch(&val,scale,scale_count-1,sizeof (float),bsearch_compare);
+    QColor gradient_by_value(const float value,const QColor* color,const qreal* scale,int scale_count) const  {
+        qreal val = min_max_float(value,scale[0],scale[scale_count-2]);
+        qreal *val_point = (qreal*) bsearch(&val,scale,scale_count-1,sizeof (qreal),bsearch_compare);
         int val_index = (int)(val_point - scale);
 
         QColor color0 = color[val_index];
@@ -106,7 +96,7 @@ private:
         result.setRedF(gradient_interpolation(color0.redF(),color1.redF(),factor));
         result.setGreenF(gradient_interpolation(color0.greenF(),color1.greenF(),factor));
         result.setBlueF(gradient_interpolation(color0.blueF(),color1.blueF(),factor));
-        result.setAlpha(200);
+        result.setAlphaF(gradient_interpolation(color0.alphaF(),color1.alphaF(),factor));
         return result;
     }
 signals:
