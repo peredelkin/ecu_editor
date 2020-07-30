857460ed071c8e90f93ece7e3c15fe1f13671f1e
Ruslan Stoyanov <ep8kha@gmail.com>
2020-07-30 13:45:47 +0500

    headerData

--- qtablemodel_ign_angle_mg_by_cycle.cpp
+++ qtablemodel_ign_angle_mg_by_cycle.cpp
@@ -37,6 +37,23 @@ bool QTableModel_ign_angle_mg_by_cycle::setData(const QModelIndex &index, const 
     table[index.row()][index.column()] = value.toFloat();
 }
 
+QVariant QTableModel_ign_angle_mg_by_cycle::headerData(int section, Qt::Orientation orientation,int role) const {
+    switch (role) {
+    case Qt::DisplayRole: {
+        switch (orientation) {
+        case Qt::Orientation::Horizontal:
+            return rpm_scale[section];
+        case Qt::Orientation::Vertical:
+            return mg_scale[section];
+        default:
+            return QVariant();
+        }
+    }
+    default:
+        return QVariant();
+    }
+}
+
 QVariant QTableModel_ign_angle_mg_by_cycle::data(const QModelIndex &index, int role) const {
     if(!index.isValid()) {
         return QVariant();
@@ -53,8 +70,6 @@ QVariant QTableModel_ign_angle_mg_by_cycle::data(const QModelIndex &index, int r
         return Qt::AlignCenter;
     case Qt::BackgroundColorRole:
         return QBrush (QColorGradientByValue::gradient(value,color_table,scale_table,7));
-    case Qt::SizeHintRole:
-        return Qt::MinimumSize;
     default:
         return QVariant();
     }
--- qtablemodel_ign_angle_mg_by_cycle.h
+++ qtablemodel_ign_angle_mg_by_cycle.h
@@ -23,6 +23,7 @@ public:
     explicit QTableModel_ign_angle_mg_by_cycle(QObject *parent = nullptr);
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
     int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant headerData(int section, Qt::Orientation orientation,int role) const;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
     bool setData(const QModelIndex &index, const QVariant &value, int role);
     Qt::ItemFlags flags(const QModelIndex &index) const;
